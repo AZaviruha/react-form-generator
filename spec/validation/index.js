3598bed6b68b3ae9e34c23fa4736ce9afe25df0f
a.zaviruha <a.zaviruha@gmail.com>
2015-05-06 18:50:24 +0300

    Update unit-tests for validation

--- spec/validation/index.js
+++ spec/validation/index.js
@@ -10,11 +10,37 @@ describe( 'validation/index.js', function () {
                 .toEqual({ 'test': null });
         });
 
-        it( 'should return error array if `validators` are set and value is not valid', function () {
+        it( 'should return array with errors if `validators` are set and value is not valid', function () {
             var validators = [{ rule: 'required', message: 'err' }];
             fldMeta.validators = validators;
+
+            expect( v.validateField( fldID, fldMeta, null ) )
+                .toEqual({ 'test': validators });
+        });
+        
+        it2( 'should return array with only those errors, that if `validators` are set ',
+             'and value is not valid', function () {
+            var validators = [{ rule: 'required', message: 'err1' },
+                              { rule: 'length', value: 5, message: 'err2' }];
+            fldMeta.validators = validators;
+
+            expect( v.validateField( fldID, fldMeta, 'test' ) )
+                .toEqual({ 'test': [ validators[1] ] });
+
             expect( v.validateField( fldID, fldMeta, null ) )
                 .toEqual({ 'test': validators });
         });
     });
 });
+
+
+function ms () {
+    return Array.prototype.join.call( arguments, '' );
+}
+
+function it2 () {
+    var ss = Array.prototype.slice.call( arguments, 0, -1 );
+    var fn = Array.prototype.slice.call( arguments, -1 )[ 0 ];
+    
+    return it(ms.apply( null, ss ), fn );
+}
