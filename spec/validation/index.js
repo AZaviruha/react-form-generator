1ed6b648febcb80e71313ec30bcfe343b163eabe
Alexei Zaviruha <a.zaviruha@gmail.com>
2015-05-12 21:43:53 +0300

    Update unit-tests for validation

--- spec/validation/index.js
+++ spec/validation/index.js
@@ -20,7 +20,7 @@ describe( 'validation/index.js', function () {
                 .toEqual({ 'test': validators });
         });
         
-        it2( 'should return array with only errors ',
+        it2( 'should return array with errors only ',
              'from failed validators', function () {
             var validators = [{ rule: 'required', message: 'err1' },
                               { rule: 'length', value: 5, message: 'err2' }];
@@ -33,6 +33,50 @@ describe( 'validation/index.js', function () {
                 .toEqual({ 'test': validators });
         });
     });
+    
+    
+    describe( 'validateForm', function () {
+        var formMeta = {
+            fields: { 
+                field1: { renderer: 'text', validators: [] }
+            }
+        };
+
+        var fldMeta = formMeta.fields.field1;
+
+
+        it2( 'should return `null` instead of errors array if ',
+            '`validators` are unset', function () {
+            expect( v.validateForm( formMeta, null ) )
+                .toEqual({ 'field1': null });
+
+            expect( v.validateForm( formMeta, { field1: '42' } ) )
+                .toEqual({ 'field1': null });
+        });
+
+        it2( 'should return array with errors if `validators` ', 
+             'are set and value is not valid', function () {
+            var validators = [{ rule: 'required', message: 'err' }];
+            fldMeta.validators = validators;
+
+            expect( v.validateForm( formMeta, null ) )
+                .toEqual({ 'field1': validators });
+        });
+        
+        it2( 'should return array with errors only ',
+             'from failed validators', function () {
+            var validators = [{ rule: 'required', message: 'err1' },
+                              { rule: 'length', value: 5, message: 'err2' }];
+            fldMeta.validators = validators;
+
+            expect( v.validateForm( formMeta, { field1: '42' } ) )
+                .toEqual({ 'field1': [ validators[1] ] });
+
+            expect( v.validateForm( formMeta, { field1: null } ) )
+                .toEqual({ 'field1': validators });
+        });
+    });
+
 });
 
 
