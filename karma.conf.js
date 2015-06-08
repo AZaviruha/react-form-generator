b9d27d884cd783b4f302cbed0447f98c038fdbad
a.zaviruha <a.zaviruha@gmail.com>
2015-06-08 13:38:45 +0300

    Remove karma-coverage. Need additional research

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-module.exports = function(config) {
+module.exports = function ( config ) {
     config.set({
         basePath:    './',
         frameworks:  [ 'jasmine' ],
@@ -8,8 +8,7 @@ module.exports = function(config) {
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-coverage'
+            'karma-jasmine'
         ],
         files: [
             { 
@@ -29,23 +28,8 @@ module.exports = function(config) {
                 served:   true
             },
             'dist/*.js'
-            // 'src/**/*.js'
-            // 'src/components/compiled/*.js',
-            // 'src/components/compiled/**/*.js'
-            // http://stackoverflow.com/questions/28236587/getting-karma-6to5ify-and-istanbul-to-play-ball
-            // http://stackoverflow.com/questions/24944541/test-coverage-with-karma-browserify-and-coffeescript
         ],
-        preprocessors: { 
-            'dist/*.js':                       [ 'coverage' ],
-            'src/**/*.js':                     [ 'coverage' ],
-            'src/components/compiled/*.js':    [ 'coverage' ],
-            'src/components/compiled/**/*.js': [ 'coverage' ] 
-        },
-        coverageReporter: {
-            type: 'lcov',
-            dir:  'coverage/'
-        },
-        reporters: [ 'progress', 'coverage' ],
+        reporters: [ 'progress' ],
         colors: true
         // logLevel: config.LOG_DEBUG
     });
